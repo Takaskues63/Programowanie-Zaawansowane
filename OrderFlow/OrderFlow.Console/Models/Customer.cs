c6c3a37eca5cd4ebb95f85330e4b0d32366b7964
Takaskues63 <bosku.student@gmail.com>
2026-04-29 21:48:15 +0300

    Remove old OrderFlow folder

--- OrderFlow/OrderFlow.Console/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-namespace OrderFlow.Console.Models;
-
-public class Customer
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
-    public bool IsVip { get; set; }
-}
--- OrderFlow/OrderFlow.Console/Models/OrderItem.cs
+++
@@ -1,9 +0,0 @@
-namespace OrderFlow.Console.Models;
-
-public class OrderItem
-{
-    public required Product Product { get; set; }
-    public int Quantity { get; set; }
-
-    public decimal TotalPrice => Product.Price * Quantity;
-}
